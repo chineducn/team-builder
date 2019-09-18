eca0c55717d9155d13a052c12207d8a335e4d87d
unknown <neduvinci@gmail.com>
2019-09-18 14:54:28 +0100

    completed step one of MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,38 +1,44 @@
 import React, { useState } from 'react';
 import './App.css';
-import uuid from uuid;
+import uuid from 'uuid';
+import TeamList from './components/TeamList';
 
 
-const team = [
+const teamReact = [
   {
     id: uuid(),
     name: 'John',
+    email: 'john@bloodyreact.com',
     specialty: 'HTML',
   }, 
   {
     id: uuid(),
     name: 'Doe',
+    email: 'doe@bloodyreact.com',
     specialty: 'Semantic HTML'
   }, 
   {
     id: uuid(),
     name: 'Jane',
+    email: 'jane@bloodyreact.com',
     specialty: 'CSS',
   },
   {
     id: uuid(),
-    name: 'Doewess',
+    name: 'Dewy',
+    email: 'dewy@bloodyreact.com',
     specialty: 'Mixins',
   }
 
 ]
 function App() {
 
-  const [teamState, setTeam] = useState([]);
+  const [teamState, setTeam] = useState(teamReact);
   const [memberState, setMember] = useState('');
   return (
     <div className="App">      
-      Bloody React      
+      Bloody React
+      <TeamList teamArray={teamState}/>
     </div>
   );
 }
