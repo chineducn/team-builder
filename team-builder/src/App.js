85ed380d2f6b7a117d2e0269d5d360727c9309d7
unknown <neduvinci@gmail.com>
2019-09-18 14:10:16 +0100

    building a team list

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import uuid from uuid;
 
+
+const team = [
+  {
+    id: uuid(),
+    name: 'John',
+    specialty: 'HTML',
+  }, 
+  {
+    id: uuid(),
+    name: 'Doe',
+    specialty: 'Semantic HTML'
+  }, 
+  {
+    id: uuid(),
+    name: 'Jane',
+    specialty: 'CSS',
+  },
+  {
+    id: uuid(),
+    name: 'Doewess',
+    specialty: 'Mixins',
+  }
+
+]
 function App() {
+
+  const [teamState, setTeam] = useState([]);
+  const [memberState, setMember] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      Bloody React      
     </div>
   );
 }
